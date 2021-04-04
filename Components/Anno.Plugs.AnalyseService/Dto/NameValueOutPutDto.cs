cdf553a673fb10ae1548a6a8e22cd2813b287b22
duyanming <dym880@163.com>
2021-04-04 19:13:21 +0800

    增加链路分析接口

---
+++ Components/Anno.Plugs.AnalyseService/Dto/NameValueOutPutDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anno.Plugs.AnalyseService.Dto
+{
+    public class NameValueOutPutDto
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+
+    }
+}
--- Components/Anno.Plugs.AnalyseService/TraceModule.cs
+++ Components/Anno.Plugs.AnalyseService/TraceModule.cs
@@ -28,5 +28,38 @@ WHERE Rlt=0 AND  Timespan>=?startDate AND Timespan<?endDate
 GROUP BY AppNameTarget; ";
             return DbInstance.Db.Ado.Query<ServiceAnalyseOutPutDto>(sql, new { startDate = startDate.ToString("yyyy-MM-dd"), endDate = endDate.ToString("yyyy-MM-dd") });
         }
+
+        [AnnoInfo(Desc = "服务管道访问统计")]
+        public dynamic ServiceModuleAnalyse([AnnoInfo(Desc = "开始时间（2021-03-30）")] DateTime startDate, [AnnoInfo(Desc = "结束时间（2021-03-31）")] DateTime endDate)
+        {
+            return GetServiceData(startDate,endDate, "Askchannel");
+        }
+
+        [AnnoInfo(Desc = "服务模块访问统计")]
+        public dynamic ServiceRouterAnalyse([AnnoInfo(Desc = "开始时间（2021-03-30）")] DateTime startDate, [AnnoInfo(Desc = "结束时间（2021-03-31）")] DateTime endDate)
+        {
+            return GetServiceData(startDate, endDate, "Askrouter");
+        }
+
+        [AnnoInfo(Desc = "服务方法访问统计")]
+        public dynamic ServiceMethodAnalyse([AnnoInfo(Desc = "开始时间（2021-03-30）")] DateTime startDate, [AnnoInfo(Desc = "结束时间（2021-03-31）")] DateTime endDate)
+        {
+            return GetServiceData(startDate, endDate, "Askmethod");
+        }
+
+        /// <summary>
+        /// Trace分析数据
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private List<NameValueOutPutDto> GetServiceData(DateTime startDate, DateTime endDate,string type) {
+
+            string sql = $@"SELECT  {type} name,COUNT({type}) value FROM  sys_trace  
+WHERE Timespan>=?startDate AND Timespan<?endDate
+GROUP BY {type}; ";
+            return DbInstance.Db.Ado.Query<NameValueOutPutDto>(sql, new { startDate = startDate.ToString("yyyy-MM-dd"), endDate = endDate.ToString("yyyy-MM-dd") });
+        }
     }
 }
